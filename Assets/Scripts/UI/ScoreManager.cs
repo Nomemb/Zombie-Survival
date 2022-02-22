cff44e5b10deab9f9eb46d016e95f8aa861efd15
Nomemb <nkj1205@gmail.com>
2022-02-22 19:14:40 +0900

    Score UI 수정
    점수 획득 시 애니메이션 구현
    스크립트 통합

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public int currentScore;
+    private int currentScore;
     private ComboManager comboManager;
+    public Text scoreTxt;
+
+
+    private Animator anim;
     private void Start()
     {
         currentScore = 0;
         comboManager = FindObjectOfType<ComboManager>();
+        anim = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        scoreTxt.text = string.Format("{0:D10}", currentScore);
+    }
     // 좀비 처치시 실행
     public void IncreaseScore(int _score)
     {
@@ -20,5 +30,6 @@ public class ScoreManager : MonoBehaviour
         // 콤보 0일때 점수 오르지 않는 현상 방지
         int _currentCombo = comboManager.CurrentCombo;
         currentScore += _score * _currentCombo;
+        anim.SetTrigger("ScoreUP");
     }
 }
