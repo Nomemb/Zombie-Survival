ccc7c5ecd5bc79540e41252e6d0a80543ebdb4b1
Nomemb <nkj1205@gmail.com>
2022-02-22 13:14:14 +0900

    Shotgun 수정
    여러 갈래로 방사형 피해 주도록 수정

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -17,6 +17,7 @@ public class Weapon : MonoBehaviour
 
     private PlayerController playerController;
     private WeaponManager weaponManager;
+
     private void Start()
     {
         playerController = FindObjectOfType<PlayerController>();
@@ -37,15 +38,40 @@ public class Weapon : MonoBehaviour
 
     IEnumerator ShootCoroutine()
     {
-        GameObject instantBullet = Instantiate(bulletPrefabs, bulletPos.position, playerController.transform.rotation);
-        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
         // 권총은 탄알이 무제한이기 때문에 예외처리함
-        if(weaponName != "Pistol")
+        if (weaponName != "Pistol")
             currentBullet -= 1;
 
-        bulletRigid.velocity = bulletPos.forward * 50;
-        yield return new WaitForSeconds(weaponRange);
-        Destroy(instantBullet);
+        if (weaponName != "Shotgun")
+        {
+            GameObject instantBullet = Instantiate(bulletPrefabs, bulletPos.position, playerController.transform.rotation);
+            Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+            // bulletPrefabs.GetComponent<Bullet>().Shoot(bulletPos.forward);
+            bulletRigid.velocity = bulletPos.forward * 50;
+            yield return new WaitForSeconds(weaponRange);
+            Destroy(instantBullet);
+
+        }
+        else
+        {
+            GameObject[] instantBullets = new GameObject[3];
+            Rigidbody[] bulletRigids = new Rigidbody[3];
+            for (int i = 0; i < instantBullets.Length; i++)
+            {
+                instantBullets[i] = Instantiate(bulletPrefabs, bulletPos.position, playerController.transform.rotation);
+                bulletRigids[i] = instantBullets[i].GetComponent<Rigidbody>();
+                Vector3 bulletDir = BoundaryAngle(10 * (i-1), bulletPos);
+                bulletRigids[i].velocity = bulletDir * 50;
+
+            }
+            
+            yield return new WaitForSeconds(weaponRange);
+            for (int i = 0; i < instantBullets.Length; i++)
+            {
+                Destroy(instantBullets[i]);
+            }
+
+        }
         yield return null;
     }
 
@@ -54,5 +80,11 @@ public class Weapon : MonoBehaviour
         Debug.Log(weaponName + " is Out of Ammo!");
         weaponManager.OutOfAmmo();
 
+    }
+
+    private Vector3 BoundaryAngle(float _angle, Transform _object)
+    {
+        _angle += _object.eulerAngles.y;
+        return new Vector3(Mathf.Sin(_angle * Mathf.Deg2Rad), 0f, Mathf.Cos(_angle * Mathf.Deg2Rad));
     }
 }
